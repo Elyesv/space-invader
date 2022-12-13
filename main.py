45e13cc797e2e1f56c3fdc8eeb783508eb869045
Elyesv <elyesvoisin52@gmail.com>
2022-12-13 09:36:03 +0100

    FIX player not showing

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import pygame, sys
-from player import Player 
+from player import Player
+ 
+class Game:
+	def __init__(self):
+		player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
+		self.player = pygame.sprite.GroupSingle(player_sprite)
 
-class Game():
-    def __init__(self):
-        player_sprite = Player((300, 300), screen_width, 5)
-        self.player = pygame.sprite.GroupSingle(player_sprite)
-
-    def run(self):
-        self.player.update()
-        
-        self.player.draw(screen)
+	def run(self):
+		self.player.update()	
+		
+		self.player.sprite.lasers.draw(screen)
+		self.player.draw(screen)
 
 if __name__ == '__main__':
-    pygame.init()
-    screen_width = 600
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width,screen_height))
-    clock = pygame.time.Clock()
-    game = Game()
+	pygame.init()
+	screen_width = 600
+	screen_height = 600
+	screen = pygame.display.set_mode((screen_width,screen_height))
+	clock = pygame.time.Clock()
+	game = Game()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
-        screen.fill((30,30,30))
-
-        pygame.display.flip
-        clock.tick(60)
+		screen.fill((30,30,30))
+		game.run()
+		#crt.draw()
+			
+		pygame.display.flip()
+		clock.tick(60)
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from laser import Laser
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,constraint,speed):
 		super().__init__()
-		self.image = pygame.image.load('../graphics/player.png').convert_alpha()
+		self.image = pygame.image.load('./assets/player.png').convert_alpha()
 		self.rect = self.image.get_rect(midbottom = pos)
 		self.speed = speed
 		self.max_x_constraint = constraint
@@ -13,9 +13,6 @@ class Player(pygame.sprite.Sprite):
 		self.laser_cooldown = 600
 
 		self.lasers = pygame.sprite.Group()
-
-		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
-		self.laser_sound.set_volume(0.5)
 
 	def get_input(self):
 		keys = pygame.key.get_pressed()
@@ -29,7 +26,6 @@ class Player(pygame.sprite.Sprite):
 			self.shoot_laser()
 			self.ready = False
 			self.laser_time = pygame.time.get_ticks()
-			self.laser_sound.play()
 
 	def recharge(self):
 		if not self.ready:
