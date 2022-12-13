95e4ed6f21f5af0ccceb8bdde4bf57dc5a328a55
Elyesv <elyesvoisin52@gmail.com>
2022-12-13 16:51:45 +0100

    CREATE alien entity and made them move

--- main.py
+++ main.py
@@ -1,34 +1,67 @@
 import pygame, sys
 from player import Player
+from alien import Alien
  
 class Game:
-	def __init__(self):
-		player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
-		self.player = pygame.sprite.GroupSingle(player_sprite)
+    def __init__(self):
+        player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+        
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows = 6, cols = 8)
+        self.alien_direction = 1
+        
+    def alien_setup(self, rows, cols, x_distance = 60, y_distance=48, x_offset= 70, y_offset = 100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+                
+                if row_index == 0: alien_sprite = Alien('green',x,y)
+                elif 1 <= row_index <= 2: alien_sprite = Alien('yellow',x,y)
+                else: alien_sprite = Alien('red',x,y)
+                self.aliens.add(alien_sprite)
 
-	def run(self):
-		self.player.update()	
-		
-		self.player.sprite.lasers.draw(screen)
-		self.player.draw(screen)
+    def alien_position_checker(self):
+        all_aliens = self.aliens.sprites()
+        for alien in all_aliens:
+            if alien.rect.right >= screen_width:
+                self.alien_direction = -1
+                self.alien_move_down(2)
+            elif alien.rect.left <= 0:
+                self.alien_direction = 1
+                self.alien_move_down(2)
+
+    def alien_move_down(self,distance):
+        if self.aliens:
+            for alien in self.aliens.sprites():
+                alien.rect.y += distance
+
+    def run(self):
+        self.player.update()	
+        self.aliens.update(self.alien_direction)
+        self.alien_position_checker()
+        self.player.sprite.lasers.draw(screen)
+        self.player.draw(screen)
+        self.aliens.draw(screen)
 
 if __name__ == '__main__':
-	pygame.init()
-	screen_width = 600
-	screen_height = 600
-	screen = pygame.display.set_mode((screen_width,screen_height))
-	clock = pygame.time.Clock()
-	game = Game()
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    clock = pygame.time.Clock()
+    game = Game()
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-		screen.fill((30,30,30))
-		game.run()
-		#crt.draw()
-			
-		pygame.display.flip()
-		clock.tick(60)
+        screen.fill((30,30,30))
+        game.run()
+        #crt.draw()
+            
+        pygame.display.flip()
+        clock.tick(60)
